19effb8f5779ec9145434c438834adb5aaccdcd1
kadoppe <kadoppe@me.com>
2015-05-08 19:39:51 +0900

    Display 20 employees each page

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,7 +2,7 @@ class EmployeesController < ApplicationController
   def index
     @employees = []
 
-    10.times do
+    20.times do
       @employees << Employee.new(
         name: FFaker::Name.name,
         department: FFaker::Education.major
