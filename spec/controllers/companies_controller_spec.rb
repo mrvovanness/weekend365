ed2579b4635a468bc47319a2b6b67395c4bc6af8
kadoppe <kadoppe@me.com>
2015-05-08 11:59:40 +0900

    Feature #2: Refactoring

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -15,11 +15,10 @@ RSpec.describe CompaniesController, type: :controller do
 
   describe 'GET #new' do
     it 'assigns @company with new company object' do
-      new_company = Company.new
-      allow(Company).to receive(:new).and_return(new_company)
+      allow(Company).to receive(:new).and_return(company)
 
       get :new
-      expect(assigns(:company)).to eq(new_company)
+      expect(assigns(:company)).to eq(company)
     end
   end
 
