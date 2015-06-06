af6778f558c7ed793bcb99c8aa75cf563b53c018
kadoppe <kadoppe@me.com>
2015-06-06 09:54:55 +0900

    Setting devise email for staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,8 +19,8 @@ Rails.application.configure do
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    domain: 'staging.365weekend.com',
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: 'heroku.com' }
+  config.action_mailer.default_url_options = { host: 'staging.365weekend.com' }
 end
