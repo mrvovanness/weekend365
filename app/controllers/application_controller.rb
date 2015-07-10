76f95374e50e6c71c691bd18c6174cec05353703
mrvovanness <mrvovanness@gmail.com>
2015-07-10 13:20:42 +0500

    Add constraints for admin namespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # Don't access admin panel for usual user
+  def authenticate_admin_user!
+    redirect_to root_path unless current_user.has_role? :admin
+  end
 end
