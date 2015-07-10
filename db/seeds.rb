219a0faf619a3b0bc043d00f370e8a5becec304b
mrvovanness <mrvovanness@gmail.com>
2015-07-10 13:27:16 +0500

    Complete add admin panel

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+
+# Create admin user
+puts 'creating user'
+user = User.new
+user.email = 'weekend365@mail.com'
+user.password = 'bigsecret'
+user.skip_confirmation!
+user.save
+user.add_role :admin
+puts "User admin? - #{user.has_role? :admin}"
+
+# Create companies
+
