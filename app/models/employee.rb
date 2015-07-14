afb4c50d3d82cfcf5e0babcfc39c440abde49143
mrvovanness <mrvovanness@gmail.com>
2015-07-14 23:08:23 +0500

    Add seed data and views

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,5 @@
 class Employee < ActiveRecord::Base
-  validates :name, :company_id, presence: true
-  validates :name, uniqueness: true
+  validates :email, uniqueness: true
   validates :email, format: { with: /\A[^@]+@[^@]+\z/ } 
 
   belongs_to :company
---
+++ db/seeds/employees.rb
@@ -0,0 +1,13 @@
+# to run this: rake db:seed:companies
+
+# Default admin company 
+company = Company.find_or_create_by(name: 'Coca-Cola') do |company|
+  company.field = 'foods'
+end
+
+puts "Creating fake employees for #{ company.name }"
+100.times do
+  company.employees.create!(name: FFaker::Name.name,
+                            email: FFaker::Internet.disposable_email )
+end
+puts "Number of fake employees created - #{ Employee.count } for company #{ company.name } of user #{company.user.email}"
