2c23cf97b0808390899675ca1756ae3728ba243d
mrvovanness <mrvovanness@gmail.com>
2015-07-17 07:53:33 +0500

    Improve fields order in company form

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -6,6 +6,21 @@ ActiveAdmin.register Company do
     actions
   end
 
+  form do |f|
+    inputs 'Details' do
+      f.input :user, as: :select, collection: User.pluck(:email, :id)
+      f.input :name
+      f.input :field
+      f.input :description
+      f.input :website
+      f.input :employees_number
+      f.input :employees_registered
+      f.input :country
+      f.input :office_address
+      actions
+    end
+  end
   permit_params :user_id, :name, :field, :office_address, :country,
     :description, :website, :employees_number, :employees_registered
+
 end
