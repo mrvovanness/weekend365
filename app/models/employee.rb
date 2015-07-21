822b68fa9c39820f382210f14b5e1d1bfe2ff218
PitOn <garik.piton@gmail.com>
2015-07-21 09:22:23 +0300

    add comment to employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,8 @@
 class Employee < ActiveRecord::Base
   validates :name, :email, presence: true
   validates :email, uniqueness: true
-  validates :email, format: { with: /\A[^@]+@[^@]+\z/ } 
+  # Как вариант использовать https://github.com/kaize/validates и тогда просто "email: true"
+  validates :email, format: { with: /\A[^@]+@[^@]+\z/ }
 
   belongs_to :company
 end
