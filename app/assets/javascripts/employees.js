320563b7cb372ed1576a2bbd756b484935841757
mrvovanness <mrvovanness@gmail.com>
2015-07-22 16:01:07 +0500

    Add select/deselect options for batch actions

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -1,5 +1,18 @@
 $(document).on('ready page:load', function() {
-  $('.bulk_actors').click(function() {
+
+  $('#select_all').click(function() {
+    if(this.checked) {
+      $('.bulk_actors').each(function() {
+        this.checked = true;
+      });
+    } else { 
+      $('.bulk_actors').each(function() {
+        this.checked = false;
+      });
+    }
+  });
+
+  $('.bulk_actors, #select_all').click(function() {
     check_count = $('.bulk_actors:checked').size();
     if (check_count > 0) {
       $('#add-to-survey-submit').show();
@@ -7,4 +20,4 @@ $(document).on('ready page:load', function() {
       $('#add-to-survey-submit').hide();
     }
   });
-})
+});
