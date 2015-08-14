b1bac04a240fca8a6c9fda136b2262375f267d48
mrvovanness <mrvovanness@gmail.com>
2015-08-15 00:21:38 +0500

    Create dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @surveys = current_user.company.surveys.includes(:answers)
+  end
 end
--- app/jobs/send_emails_job.rb
+++ app/jobs/send_emails_job.rb
@@ -5,13 +5,11 @@ class SendEmailsJob
     survey = Survey.find(survey_id)
     survey.employees.find_each do |employee|
       SurveysMailer.send_survey(survey, employee.email).deliver_now
-      puts "mail to #{employee.name} was delivered"
     end
     survey.update_column(:counter, survey.counter += 1)
 
     if survey.counter == survey.number_of_repeats
       Resque.remove_schedule("send_emails_for_survey_#{survey.id}")
     end
-    puts "send number #{survey.counter} from #{survey.number_of_repeats}"
   end
 end
