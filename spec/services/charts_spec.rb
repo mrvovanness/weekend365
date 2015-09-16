376fa6644719b8e552ef1c2199e9a331195b27a9
mrvovanness <mrvovanness@gmail.com>
2015-09-16 20:31:25 +0500

    Clean charts_spec

--- spec/services/charts_spec.rb
+++ spec/services/charts_spec.rb
@@ -3,17 +3,4 @@ require 'rails_helper'
 describe ChartsService do
   let!(:survey) { build(:survey_with_finish_on) }
 
-  it 'calculate one time per 2 weeks' do
-    survey.repeat_mode = 'w'
-    survey.repeat_every = 2
-    survey.start_at = DateTime.now
-    expect(RepeatsCalculator.new(survey).result).to eq(26)
-  end
-
-  it 'calculate one time per 2 days' do
-    survey.repeat_mode = 'd'
-    survey.repeat_every = 2
-    survey.start_at = DateTime.now
-    expect(RepeatsCalculator.new(survey).result).to eq(183)
-  end
 end
