53c30df8cf2d40d481033aec621e043371e017d0
mrvovanness <mrvovanness@gmail.com>
2015-10-03 14:48:05 +0500

    Fix specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
     email 'ex@mail.com'
+    name FFaker::Name.name
     password 'bigsecret'
     password_confirmation { password }
     confirmed_at Time.zone.now
