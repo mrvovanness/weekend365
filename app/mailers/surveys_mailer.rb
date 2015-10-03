0fd73a0c337080243469b0ccf29f497f308cc9c1
mrvovanness <mrvovanness@gmail.com>
2015-10-03 17:10:20 +0500

    Fix mailer

--- app/mailers/surveys_mailer.rb
+++ app/mailers/surveys_mailer.rb
@@ -1,9 +1,9 @@
 class SurveysMailer < ActionMailer::Base
 
   def send_survey(survey, employee)
+    @survey = survey
     I18n.locale = (@survey.locale || 'en').to_sym
     token = survey.tokens.create(name: SecureRandom.hex(10), expired: false)
-    @survey = survey
     @answers = @survey.offered_questions.first.offered_answers
     @employee = employee
     @promo = Article.find_by(title: 'Email Promo')
