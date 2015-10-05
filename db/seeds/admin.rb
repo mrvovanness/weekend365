f12646eb53319e6c0d1a815cd267285da2c4d57c
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-10-05 21:51:45 +0700

    Fix db:seed:admin

--- db/seeds/admin.rb
+++ db/seeds/admin.rb
@@ -1,16 +1,18 @@
 # Create admin user
 puts 'creating signup'
-signup = Signup.new
-signup.name = 'Gustavo'
-signup.email = 'weekend@mail.com'
-signup.password = 'bigsecret'
-signup.company_name = 'Brazilian Tunes'
-signup.country = 'Brazil'
-signup.save
 
-user = User.find_by(name: 'Gustavo')
-user.confirmed_at = DateTime.current - 1.day
+company = Company.new
+company.name = 'Brazilian Tunes'
+company.country = 'Brazil'
+company.save
+
+user = company.users.new
+user.name = 'Gustavo'
+user.email = 'weekend@mail.com'
+user.skip_confirmation!
+user.password = 'bigsecret'
 user.save
+
 user.add_role :admin
 
 puts "User admin? - #{user.has_role? :admin}, with company #{user.company.name}"
