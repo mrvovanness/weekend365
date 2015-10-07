f85791f19effbb1aa486667e3dcc5a1da303a8d6
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-10-07 10:50:51 +0700

    Add new style for homepage

--- db/seeds/articles.rb
+++ db/seeds/articles.rb
@@ -14,4 +14,20 @@ privacy_policy = Article.find_or_create_by(title: 'Privacy Policy') do |a|
   a.body_markdown = '##coming soon'
 end
 
+privacy_policy = Article.find_or_create_by(title: 'Advertisement') do |a|
+  a.body_markdown = '###Are employees leaving too soon?'
+end
+
+privacy_policy = Article.find_or_create_by(title: 'Company culture') do |a|
+  a.body_markdown = '###Company culture'
+end
+
+privacy_policy = Article.find_or_create_by(title: 'Compare') do |a|
+  a.body_markdown = '###Compare'
+end
+
+privacy_policy = Article.find_or_create_by(title: 'We help you') do |a|
+  a.body_markdown = '###We help you'
+end
+
 puts "Created: #{ Article.all.map(&:title).join(', ')}"
