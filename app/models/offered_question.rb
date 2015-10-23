20547694149a79d4eb088e53b502b14edd99aeca
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-10-23 09:47:40 +0700

    Set default answers if they are upsent

--- app/models/offered_question.rb
+++ app/models/offered_question.rb
@@ -6,10 +6,20 @@ class OfferedQuestion < ActiveRecord::Base
   has_many :offered_answers, through: :sqa_assignments
   has_many :offered_surveys, through: :sqa_assignments
 
+  after_create :set_default_answers
+
   translates :title, :topic, :subtopic
 
   def self.import(file)
     spreadsheet = Spreadsheet.new(file)
     spreadsheet.import(self.model_name.name)
+  end
+
+  private
+
+  def set_default_answers
+    if self.offered_answers.empty?
+      self.offered_answers = OfferedAnswer.all
+    end
   end
 end
