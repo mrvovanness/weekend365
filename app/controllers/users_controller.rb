6c94749db9b7c400022b4ed46e980a35c3c1459f
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-11-09 17:51:37 +0700

    Add profile form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,20 @@ class UsersController < ApplicationController
     respond_with @user
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    user = current_user
+    if user.update(user_params)
+      sign_in(user, bypass: true)
+      respond_with user, location: -> { surveys_path }
+    else
+      render :edit
+    end
+  end
+
   def destroy_selected
     User.destroy(params[:user_ids])
     respond_with @user
@@ -23,6 +37,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
